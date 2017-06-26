975b754389bba6afdc779a72d17ccc88622eb0fc
wpcfan <peng.wang@outlook.com>
2017-06-27 00:22:53 +0800

    add xlsx support for jasperreports

--- src/main/java/dev/local/reports/ReportXlsController.java
+++ src/main/java/dev/local/reports/ReportXlsController.java
@@ -8,14 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ReportController {
-
-    @RequestMapping(path = "/pdf", method = RequestMethod.GET)
+public class ReportXlsController {
+    @RequestMapping(path = "/xls", method = RequestMethod.GET)
     public ModelAndView report(ModelMap modelMap, ModelAndView modelAndView) {
         modelMap.put("datasource", new JREmptyDataSource());
         modelMap.put("title", "王芃");
-        modelAndView = new ModelAndView("pdf", modelMap);
+        modelAndView = new ModelAndView("xls", modelMap);
         return modelAndView;
     }
 }
-
--- src/main/java/dev/local/secruity/WebSecurityConfig.java
+++ src/main/java/dev/local/secruity/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                         "/**/*.js"
                 ).permitAll()
                 // 对于获取token的rest api要允许匿名访问
-                .antMatchers("/auth/**", "/pdf/**").permitAll()
+                .antMatchers("/auth/**", "/pdf/**", "/xls/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
 
