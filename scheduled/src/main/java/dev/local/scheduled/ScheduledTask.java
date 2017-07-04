f9a98881f719f4d76b5c16a0ba8907667e1eab99
wpcfan <peng.wang@outlook.com>
2017-07-04 22:04:27 +0800

    add schedule sub-project

---
+++ scheduled/src/main/java/dev/local/scheduled/ScheduledTask.java
@@ -0,0 +1,21 @@
+package dev.local.scheduled;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTask {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(cron="0/5 * *  * * ? ")
+    public void reportCurrentTime() {
+        log.info("The time is {}", dateFormat.format(new Date()));
+    }
+}
