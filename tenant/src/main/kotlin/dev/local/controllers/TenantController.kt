a40ff74308c7502c3186c4f97fd4bf939ace9522
wpcfan <peng.wang@outlook.com>
2017-09-09 19:15:57 +0800

    refactor the dependencies

---
+++ tenant/src/main/kotlin/dev/local/controllers/TenantController.kt
@@ -0,0 +1,8 @@
+package dev.local.controllers
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/projects")
+class TenantController
