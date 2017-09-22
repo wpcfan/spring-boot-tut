744b284bf045c093b9f14532489f4f2cba21bada
wpcfan <peng.wang@outlook.com>
2017-09-23 00:39:39 +0800

    add swagger

--- api/src/main/java/dev/local/Application.java
+++ api/src/main/java/dev/local/Application.java
@@ -2,14 +2,44 @@ package dev.local;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Spring Boot 应用的入口文件
  * @Author Peng Wang
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public Docket simpleDiffServiceApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("taskmgr")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+            .pathMapping("/");
+
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("A Task Manager")
+                .description("A simple calculator REST service made with Spring Boot in Java")
+                .contact(new Contact("Peng Wang", "http://github.com/wpcfan", "wpcfan@gmail.com"))
+                .version("1.0")
+                .build();
     }
 }
