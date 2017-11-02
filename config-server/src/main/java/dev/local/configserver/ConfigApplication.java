3367f5e40cb7bee54b5202190fb21dfcfb862fa4
wpcfan <peng.wang@outlook.com>
2017-11-03 00:56:07 +0800

    dash - cannot be in the name

--- config-server/src/main/java/dev/local/configserver/ConfigApplication.java
+++ config-server/src/main/java/dev/local/configserver/ConfigApplication.java
@@ -2,10 +2,14 @@ package dev.local.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
+@Configuration
 public class ConfigApplication {
     /**
      * The main method.
