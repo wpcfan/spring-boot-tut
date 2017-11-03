c18e9334d7416ffb70f66bd5d678128d5ed7e9bf
wpcfan <peng.wang@outlook.com>
2017-11-03 12:04:30 +0800

    restructure

--- zipkin-server/bin/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# The environment variables are already set up by the Dockerfile
-java -Djava.security.egd=file:/dev/urandom -Dspring.profiles.active=docker -jar ${APP_JAR_NAME}-${APP_JAR_VERSION}.jar
